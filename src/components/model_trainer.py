e36a7bacc76f630964a66dd71f5342ff52aa4fc6
B0scos <lol.lol.lol.matheus@gmail.com>
2025-12-19 13:59:35 -0300

    Initiation of basic classes whitin model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,3 +19,23 @@ from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train, y_train = train_array[:, :-1], train_array[:, -1]
+            X_test, y_test = test_array[:, :-1], test_array[:, -1]
+
+            
+            
+        except Exception as e:
+            raise CustomException(e, sys)

c7d997bbdd5e23a716929538abf84f64e7a34117
B0scos <lol.lol.lol.matheus@gmail.com>
2025-12-19 14:59:55 -0300

    model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,10 +14,10 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
-from src.exception import CustomException
+from src.exception import CostumException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -46,6 +46,20 @@ class ModelTrainer:
             }
 
             model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, models=models, X_test=X_test, y_test=y_test)
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+            logging.info(f"Best model found: {best_model_name} with r2 score: {best_model_score}")
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
             
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CostumException(e, sys)
